d26a8e09045a380f8f2c74a0fca98cadd6ce0561
Bárbara Fernandes <barbara.dcf@gmail.com>
2015-10-04 12:06:50 -0300

    Product Model Specs created

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :product do
-    name "MyString"
-category "MyString"
-price "9.99"
-description "MyText"
+    name "Cachecol azul com pompons"
+    category "Roupas"
+    price "12.99"
+    confection_time_in_hours "3.5"
+    description "Um cachecol azul com pompons muito bonito."
   end
-
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Product, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(FactoryGirl.create(:product)).to be_valid
+  end
+  it "is invalid without a name" do
+    expect(FactoryGirl.build(:product, name:nil)).to_not be_valid
+  end
+  it "is invalid without a category" do
+    expect(FactoryGirl.build(:product, category:nil)).to_not be_valid
+  end
+  it "is invalid without a price" do
+    expect(FactoryGirl.build(:product, price:nil)).to_not be_valid
+  end
+  it "is invalid without a confection_time_in_hours" do
+    expect(FactoryGirl.build(:product, confection_time_in_hours:nil)).to_not be_valid
+  end
+  it "is valid without a description" do
+    expect(FactoryGirl.build(:product, description:nil)).to be_valid
+  end
 end
